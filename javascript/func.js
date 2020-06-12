99146446b8d58883e7d7c899e11cc24e0ede9ebf
VictorMacabu <54696589+VictorMacabu@users.noreply.github.com>
2020-06-11 22:57:31 -0300

    Add files via upload

--- javascript/func.js
+++ javascript/func.js
@@ -3,4 +3,8 @@
   }
   function voltaimg(){
     document.getElementById("Logo").src="img/292183.png";
+  } 
+  function passatxt(){
+  var text1=document.forms["nTabTraduc"].elements[0].value
+    document.getElementById("iTextsai").innerHTML = text1;
   }
