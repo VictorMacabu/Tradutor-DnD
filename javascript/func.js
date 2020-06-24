75b59802397970d3915f3fe3bf18f15f3c4af70f
VictorMacabu <54696589+VictorMacabu@users.noreply.github.com>
2020-06-24 01:00:03 -0300

    Add files via upload

--- javascript/func.js
+++ javascript/func.js
@@ -8,3 +8,23 @@
   var text1=document.forms["nTabTraduc"].elements[0].value
     document.getElementById("iTextsai").innerHTML = text1;
   }
+  var select = document.querySelector('select');
+  
+  select.addEventListener('change', selectfont);
+  
+  function selectfont(){
+   var muda=select.value
+   var font1=document.getElementById("iTextsai").style.fontStyle = "italic"; 
+   var font2=document.getElementById("iTextsai").style.fontFamily = "DavekFontAnoes";
+   var font3=document.getElementById("iTextsai").style.fontFamily = "IokharicDraconic";
+   var font4=document.getElementById("iTextsai").style.fontFamily = "RelanicFontElfica";
+   if (muda==='comum') {
+    text1=font1;
+   } else if (muda==='Anoico') {
+    text1=font2;
+   } else if (muda==='Dracônico') {
+    text1=font3;
+   } else if( muda==='Elfico') {
+    text1=font4;
+   }
+}
