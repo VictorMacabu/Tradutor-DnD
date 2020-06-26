0244b02e58d5ab4fa39b960f51ecaa2413cd2f3b
VictorMacabu <54696589+VictorMacabu@users.noreply.github.com>
2020-06-25 23:35:54 -0300

    Add files via upload

--- javascript/func.js
+++ javascript/func.js
@@ -7,24 +7,25 @@
   function passatxt(){
   var text1=document.forms["nTabTraduc"].elements[0].value
     document.getElementById("iTextsai").innerHTML = text1;
+  }  
+  function selectfont1(){ 
+    var font1 = document.getElementById("iTextsai").style.fontStyle = "italic"; 
+     document.getElementById("iTextsai").style.fontSize = "30px";
+     document.getElementById("iTextsai").style.fontFamily = "arial";
+     font1
+    }
+  function selectfont2(){
+    var font2 = document.getElementById("iTextsai").style.fontFamily = "DavekFontAnoes";
+    document.getElementById("iTextsai").style.fontSize = "30px";
+    font2 
   }
-  var select = document.querySelector('select');
-  
-  select.addEventListener('change', selectfont);
-  
-  function selectfont(){
-   var muda=select.value
-   var font1=document.getElementById("iTextsai").style.fontStyle = "italic"; 
-   var font2=document.getElementById("iTextsai").style.fontFamily = "DavekFontAnoes";
-   var font3=document.getElementById("iTextsai").style.fontFamily = "IokharicDraconic";
-   var font4=document.getElementById("iTextsai").style.fontFamily = "RelanicFontElfica";
-   if (muda==='comum') {
-    text1=font1;
-   } else if (muda==='Anoico') {
-    text1=font2;
-   } else if (muda==='Dracônico') {
-    text1=font3;
-   } else if( muda==='Elfico') {
-    text1=font4;
-   }
-}
+    function selectfont3(){
+    var font3 = document.getElementById("iTextsai").style.fontFamily = "IokharicDraconic";
+    document.getElementById("iTextsai").style.fontSize = "30px";
+    font3
+  }
+    function selectfont4(){
+    var font4 = document.getElementById("iTextsai").style.fontFamily = "RelanicFontElfica";
+    document.getElementById("iTextsai").style.fontSize = "30px";
+    font4 
+  }
