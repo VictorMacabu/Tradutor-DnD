6fdd766c114b6f54dd8d2c8b3bab16c4e53284da
VictorMacabu <54696589+VictorMacabu@users.noreply.github.com>
2021-04-01 18:18:22 -0300

    Add files via upload

--- javascript/func.js
+++ javascript/func.js
@@ -10,22 +10,22 @@
   }  
   function selectfont1(){ 
     var font1 = document.getElementById("iTextsai").style.fontStyle = "italic"; 
-     document.getElementById("iTextsai").style.fontSize = "30px";
+     document.getElementById("iTextsai").style.fontSize = "22px";
      document.getElementById("iTextsai").style.fontFamily = "arial";
      font1
-    }
+  }
   function selectfont2(){
     var font2 = document.getElementById("iTextsai").style.fontFamily = "DavekFontAnoes";
-    document.getElementById("iTextsai").style.fontSize = "30px";
+    document.getElementById("iTextsai").style.fontSize = "22px";
     font2 
   }
     function selectfont3(){
     var font3 = document.getElementById("iTextsai").style.fontFamily = "IokharicDraconic";
-    document.getElementById("iTextsai").style.fontSize = "30px";
+    document.getElementById("iTextsai").style.fontSize = "22px";
     font3
   }
     function selectfont4(){
     var font4 = document.getElementById("iTextsai").style.fontFamily = "RelanicFontElfica";
-    document.getElementById("iTextsai").style.fontSize = "30px";
+    document.getElementById("iTextsai").style.fontSize = "22px";
     font4 
   }
